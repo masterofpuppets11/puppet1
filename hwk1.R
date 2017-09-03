e4758beba34824c48a8aba98b8d88228d325005b
Bharath <bharathrupani@outlook.com>
2017-09-03 23:22:20 +0800

    Update till 5

--- hwk1.R
+++ hwk1.R
@@ -28,3 +28,28 @@ qplot(DHSILR, main = "Histogram of Log Returns for HSI") + geom_histogram(bins =
 # 3. Histogram with superimposed normal curve
 qplot(DHSILR, main = "Histogram of Log Returns for HSI") + geom_histogram(bins = 100, binwidth=0.01)+geom_density(aes(DHSILR), color="gray", na.rm = TRUE)
 
+# 4. KDE with different kernals
+par(mfrow=c(2,2))
+KD5 <- density(DHSILR, kernel = "gaussian", bw = .005) 
+plot(KD5, type="l",ylim=c(0,55), main="KDE of DSPLR with Gaussian Kernel")
+KD6 <- density(DHSILR, kernel = "rectangular", bw = .005) 
+plot(KD6, type="l",ylim=c(0,55), main="KDE of DSPLR with rectangular Kernel")
+KD7 <- density(DHSILR, kernel = "triangular", bw = .005) 
+plot(KD7, type="l",ylim=c(0,55), main="KDE of DSPLR with triangular Kernel")
+KD8 <- density(DHSILR, kernel = "cosine", bw = .005) 
+plot(KD8, type="l", ylim=c(0,55),main="KDE of DSPLR with cosine Kernel")
+
+qplot(DHSILR, main = "Different Kernals", geom="density")+stat_density(kernal="gaussian", bw=0.005, show.legend = TRUE, position = "stack", adjust=1)
+qplot(DHSILR, main = "Different Kernals", geom="density")+stat_density(kernal="rectangular", bw=0.005, show.legend = TRUE, position = "stack", adjust=1)
+
+
+# 5. KDE with different
+par(mfrow=c(2,2))
+KD1 <- density(DSPLR, kernel = "gaussian", bw = .01) 
+plot(KD1, type="l",ylim=c(0,65), main="KDE of DSPLR with BW=.01")
+KD2 <- density(DSPLR, kernel = "gaussian", bw = .005) 
+plot(KD2, type="l",ylim=c(0,65), main="KDE of DSPLR with BW=.005")
+KD3 <- density(DSPLR, kernel = "gaussian", bw = .001) 
+plot(KD3, type="l",ylim=c(0,65), main="KDE of DSPLR with BW=.001")
+KD4 <- density(DSPLR, kernel = "gaussian", bw = .00001) 
+plot(KD4, type="l", main="KDE of DSPLR with BW=.00001")
